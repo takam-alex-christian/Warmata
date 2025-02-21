244543367a12fe2bec6fab3f4e20952ff2fbed3f
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-22 03:25:37 +0800

    Displaying stores

--- src/features/GameScreenFeature/index.tsx
+++ src/features/GameScreenFeature/index.tsx
@@ -5,14 +5,10 @@ import { useContext } from "react";
 import GameContext from "@/lib/GameContext";
 
 import CharacterView from "../CharacterView";
+import StoresView from "../StoresView";
 
 function GameScreenFeature() {
   const { gameState, gameDispatch } = useContext(GameContext);
-
-  function handleClassChange() {
-    //
-    gameDispatch({ type: "changed_next_class" });
-  }
 
   return (
     <div
@@ -20,8 +16,9 @@ function GameScreenFeature() {
         gameState.currentClass == "taoist" ? "bg-[#C3C8D5]" : "bg-white"
       } h-full py-10`}
     >
-      <div className="w-4/5 mx-auto">
+      <div className="w-4/5 mx-auto flex flex-row gap-40">
         <CharacterView />
+        <StoresView />
       </div>
     </div>
   );
