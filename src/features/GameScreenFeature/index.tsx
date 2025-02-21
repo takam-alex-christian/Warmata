242f5114f9f4c86cd7b5fc95e0a12f16cddd7ddb
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-22 04:44:46 +0800

    Adding a title and my signature

--- src/features/GameScreenFeature/index.tsx
+++ src/features/GameScreenFeature/index.tsx
@@ -14,11 +14,23 @@ function GameScreenFeature() {
     <div
       className={`${
         gameState.currentClass == "taoist" ? "bg-[#C3C8D5]" : "bg-white"
-      } h-full py-10`}
+      } relative h-full pt-10 flex flex-col`}
     >
+      <div className="mb-20">
+        <h1 className="text-4xl font-bold text-center">
+          The Return Of WARMATA
+        </h1>
+        <h3 className="text-xl font-medium text-center">
+          Warrior - Mage - Taoist
+        </h3>
+      </div>
+
       <div className="w-4/5 mx-auto flex flex-row gap-40">
         <CharacterView />
         <StoresView />
+      </div>
+      <div className="h-full pb-10 flex flex-col">
+        <p className="m-auto ">Develoed with ❤️ by Alex Christian Takam.</p>
       </div>
     </div>
   );
